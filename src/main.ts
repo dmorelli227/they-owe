833ea637a0228f0d437e80d34d552d87b13e7e35
Daniele Morelli <dmorelli227@gmail.com>
2020-06-03 20:46:31 -0400

    feat: setup heroku confiuration from initial deploy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path'
+import { NestExpressApplication } from '@nestjs/platform-express'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  app.useStaticAssets(join(__dirname, '..', 'public'))
+  app.setBaseViewsDir(join(__dirname, '..', 'views'))
+  app.setViewEngine('ejs')
+  // tslint:disable-next-line:no-console
+  await app.listen(process.env.PORT || 3000)
 }
-bootstrap();
+bootstrap()
